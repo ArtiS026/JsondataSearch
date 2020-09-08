88248fcc19d198fefd928a3bf2cbaf206cd2ed3e
Arti Singh <arti.a.singh@capgemini.com>
2020-09-08 19:09:58 +0200

    Add project files.

---
+++ JsonDataSearch/Controllers/DataSearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonDataSearch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataSearch : ControllerBase
+    {
+        private IHostingEnvironment _env;
+        public DataSearch(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        // GET api/values
+        //Calling html file 
+        [HttpGet]
+        [Produces("text/html")]
+        public ActionResult<string> GetJsonData()
+        {
+            var webRoot = _env.WebRootPath;
+            var fileContent = System.IO.File.ReadAllText(webRoot + "/SearchValue.html");
+            return base.Content(fileContent, "text/html");
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string name)
+        {
+            return "";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ JsonDataSearch/wwwroot/SearchFunctionality.js
@@ -0,0 +1,25 @@
+﻿
+
+$(document).ready(function () {
+   
+    $.ajaxSetup({ cache: false });
+    $('#search').keyup(function () {
+        $('#result').html('');
+        $('#state').val('');
+        var searchField = $('#search').val();
+        var expression = new RegExp(searchField, "i");
+        $.getJSON('rapunzelproducts.json', function (data) { 
+            $.each(data, function (key, value) {
+                if (value.name.search(expression) != -1) { 
+                    $('#result').append(+ value.name + ' | <span class="text-muted">' + value.color - calc + '</span></li>');
+                }
+            });
+        });
+    });
+
+    $('#result').on('click', 'li', function () {
+        var click_text = $(this).text().split('|');
+        $('#search').val($.trim(click_text[0]));
+        $("#result").html('');
+    });
+});
