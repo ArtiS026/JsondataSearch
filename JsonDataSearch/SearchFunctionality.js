61c22214034b8f0fce61eeef6814e7c2079b1d79
ArtiS026 <44610102+ArtiS026@users.noreply.github.com>
2020-09-08 20:16:28 +0200

    Add files via upload

---
+++ JsonDataSearch/SearchFunctionality.js
@@ -0,0 +1,26 @@
+﻿
+
+$(document).ready(function () {
+   
+    $.ajaxSetup({ cache: false });
+    $('#search').keyup(function () {
+        $('#result').html('');
+        $('#state').val('');
+        var searchField = $('#search').val();
+        var expression = new RegExp(searchField, "i");
+        $.getJSON('rapunzelproducts.json', function (data) { 
+            $.each(data, function (key, value) {
+                if (value.name.search(expression) != -1) { 
+                    $('#result').append(+ value.name + ' | <span>' + value.color - calc + '</span> '
+                        + ' | <span>' + value.color_caption - calc + '</span></li>');
+                }
+            });
+        });
+    });
+
+    $('#result').on('click', 'li', function () {
+        var click_text = $(this).text().split('|');
+        $('#search').val($.trim(click_text[0]));
+        $("#result").html('');
+    });
+});
